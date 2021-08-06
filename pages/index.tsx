0e7afe264e221074052476eeab48f800021b551c
waymao <my.waymao+git@gmail.com>
2021-08-06 14:48:48 +0800

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Container from '../components/container'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
-import { getIntro } from '../lib/get_intro'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import PostBody from '../components/post-body'
 import SelfIntro from '../components/self-intro'
 
 export default function Index({ content }) {
